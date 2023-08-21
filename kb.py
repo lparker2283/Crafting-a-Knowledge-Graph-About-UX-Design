dbfc1939cf5e8b72a47b16cbb83487c21234a884
Lindsey <74312407+lparker2283@users.noreply.github.com>
2023-08-21 10:51:15 -0600

    Add files via upload
    Adding final main.py and knowledge graph class (kb.py) files, as well as an interactive graph constructed with pyvis.

---
+++ kb.py
@@ -0,0 +1,102 @@
+import wikipedia  # Add this line to import the wikipedia module
+
+class KB():
+    def __init__(self):
+        self.relations = []
+        self.entities = {}
+        # meta: involves article URLs
+        self.sources = {} 
+    
+    def merge_with_kb(self, kb2):
+        for r in kb2.relations:
+            article_url = list(r["meta"].keys())[0]
+            source_data = kb2.sources[article_url]
+            self.add_relation(r, source_data["article_title"], source_data["article_publish_date"])
+
+    def are_relations_equal(self, r1, r2):
+        return all(r1[attr] == r2[attr] for attr in ["head", "type", "tail"])
+
+    def exists_relation(self, r1):
+        return any(self.are_relations_equal(r1, r2) for r2 in self.relations)
+
+    def merge_relations(self, r2):
+        r1 = [r for r in self.relations
+              if self.are_relations_equal(r2, r)][0]
+        
+        # if different article
+        article_url = list(r2["meta"].keys())[0]
+        if article_url not in r1["meta"]:
+            r1["meta"][article_url] = r2["meta"][article_url]
+        
+        # if existing article
+        else: 
+            spans_to_add = [span for span in r2["meta"][article_url]["spans"]
+                            if span not in r1["meta"][article_url]["spans"]]
+            r1["meta"][article_url]["spans"] += spans_to_add
+
+    def get_wikipedia_data(self, candidate_entity):
+        try:
+            page = wikipedia.page(candidate_entity, auto_suggest=False)
+            entity_data = {
+                "title": page.title,
+                "url": page.url,
+                "summary": page.summary
+            }
+            return entity_data
+        except:
+            return None
+
+    def add_entity(self, e):
+        self.entities[e["title"]] = {k: v for k, v in e.items() if k != "title"}
+
+    def add_relation(self, r, article_title, article_publish_date):
+        # check on wikipedia
+        candidate_entities = [r["head"], r["tail"]]
+        entities = [self.get_wikipedia_data(ent) for ent in candidate_entities]
+
+        # if one entity does not exist, stop
+        if any(ent is None for ent in entities):
+            return
+
+        # manage new entities
+        for e in entities:
+            self.add_entity(e)
+
+        # rename relation entities with their wikipedia titles
+        r["head"] = entities[0]["title"]
+        r["tail"] = entities[1]["title"]
+
+        # add source if not in kb
+        article_url = list(r["meta"].keys())[0]
+        if article_url not in self.sources: 
+            self.sources[article_url] = {
+                "article_title": article_title,
+                "article_publish_date": article_publish_date
+            }
+
+        # manage new relation
+        if not self.exists_relation(r):
+           self.relations.append(r)
+        else:
+           self.merge_relations(r)
+
+    def print(self):
+        print("Entities:")
+        for e in self.entities.items():
+            print(f" {e}")
+        print("Relations:")
+        relation_count = len(self.relations)
+        print(f"  Total Relations: {relation_count}")
+        for r in self.relations:
+            print(f"  {r}")
+        print("Sources:")
+        for s in self.sources.items():
+            print(f" {s}")
+    
+    # method added to merge multiple knowledge bases for separate articles together
+    def merge_with_kb(self, kb2):
+        for r in kb2.relations:
+            article_url = list(r["meta"].keys())[0]
+            source_data = kb2.sources[article_url]
+            self.add_relation(r, source_data["article_title"],
+                              source_data["article_publish_date"])
---
+++ main.py
@@ -0,0 +1,232 @@
+# Import libraries
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+import math
+import torch
+from newspaper import Article, ArticleException
+from GoogleNews import GoogleNews
+import IPython
+from pyvis.network import Network
+from kb import KB  # Import the KB class from kb.py
+import wikipedia
+import os
+from IPython.display import IFrame
+
+# Load model and tokenizer
+tokenizer = AutoTokenizer.from_pretrained("Babelscape/rebel-large")
+model = AutoModelForSeq2SeqLM.from_pretrained("Babelscape/rebel-large")
+
+def extract_relations_from_model_output(text):
+    relations = []
+    relation, subject, relation, object_ = '', '', '', ''
+    text = text.strip()
+    current = 'x'
+    text_replaced = text.replace("<s>", "").replace("<pad>", "").replace("</s>", "")
+    for token in text_replaced.split():
+        if token == "<triplet>":
+            current = 't'
+            if relation != '':
+                relations.append({
+                    'head': subject.strip(),
+                    'type': relation.strip(),
+                    'tail': object_.strip()
+                })
+                relation = ''
+            subject = ''
+        elif token == "<subj>":
+            current = 's'
+            if relation != '':
+                relations.append({
+                    'head': subject.strip(),
+                    'type': relation.strip(),
+                    'tail': object_.strip()
+                })
+            object_ = ''
+        elif token == "<obj>":
+            current = 'o'
+            relation = ''
+        else:
+            if current == 't':
+                subject += ' ' + token
+            elif current == 's':
+                object_ += ' ' + token
+            elif current == 'o':
+                relation += ' ' + token
+    if subject != '' and relation != '' and object_ != '':
+        relations.append({
+            'head': subject.strip(),
+            'type': relation.strip(),
+            'tail': object_.strip()
+        })
+    return relations
+
+def from_small_text_to_kb(text, verbose=False):
+    kb = KB()
+
+    # Tokenizer text
+    model_inputs = tokenizer(text, max_length=512, padding=True, truncation=True,
+                            return_tensors='pt')
+    if verbose:
+        print(f"Num tokens: {len(model_inputs['input_ids'][0])}")
+
+    # Generate
+    gen_kwargs = {
+        "max_length": 216,
+        "length_penalty": 0,
+        "num_beams": 3,
+        "num_return_sequences": 3
+    }
+    generated_tokens = model.generate(
+        **model_inputs,
+        **gen_kwargs,
+    )
+    decoded_preds = tokenizer.batch_decode(generated_tokens, skip_special_tokens=False)
+
+    # create kb
+    for sentence_pred in decoded_preds:
+        relations = extract_relations_from_model_output(sentence_pred)
+        for r in relations:
+            kb.add_relation(r)
+
+    return kb
+
+# Altered method to extract kb from whole article
+def from_text_to_kb(text, article_url, span_length=128, article_title=None,
+                    article_publish_date=None, verbose=False):
+    # tokenize whole text
+    inputs = tokenizer([text], return_tensors="pt")
+
+    # compute span boundaries
+    num_tokens = len(inputs["input_ids"][0])
+    if verbose:
+        print(f"Input has {num_tokens} tokens")
+    num_spans = math.ceil(num_tokens / span_length)
+    if verbose:
+        print(f"Input has {num_spans} spans")
+    overlap = math.ceil((num_spans * span_length - num_tokens) / 
+                        max(num_spans - 1, 1))
+    spans_boundaries = []
+    start = 0
+    for i in range(num_spans):
+        spans_boundaries.append([start + span_length * i,
+                                 start + span_length * (i + 1)])
+        start -= overlap
+    if verbose:
+        print(f"Span boundaries are {spans_boundaries}")
+
+    # transform input with spans
+    tensor_ids = [inputs["input_ids"][0][boundary[0]:boundary[1]]
+                  for boundary in spans_boundaries]
+    tensor_masks = [inputs["attention_mask"][0][boundary[0]:boundary[1]]
+                    for boundary in spans_boundaries]
+    inputs = {
+        "input_ids": torch.stack(tensor_ids),
+        "attention_mask": torch.stack(tensor_masks)
+    }
+
+    # generate relations
+    num_return_sequences = 3
+    gen_kwargs = {
+        "max_length": 256,
+        "length_penalty": 0,
+        "num_beams": 3,
+        "num_return_sequences": num_return_sequences
+    }
+    generated_tokens = model.generate(
+        **inputs,
+        **gen_kwargs,
+    )
+
+    # decode relations
+    decoded_preds = tokenizer.batch_decode(generated_tokens,
+                                           skip_special_tokens=False)
+    
+    # create kb
+    kb = KB()
+    i = 0
+    for sentence_pred in decoded_preds:
+        current_span_index = i // num_return_sequences
+        relations = extract_relations_from_model_output(sentence_pred)
+        for relation in relations:
+            relation["meta"] = {
+                article_url: {
+                    "spans": [spans_boundaries[current_span_index]]
+                }
+            }
+            kb.add_relation(relation, article_title, article_publish_date)
+        i += 1
+
+    return kb
+
+# new methods for fetching url, downloading, parsing, and constructing kb
+def get_article(url):
+    article = Article(url)
+    article.download()
+    article.parse()
+    return article
+
+def from_url_to_kb(url):
+    article = get_article(url)
+    config = {
+        "article_title": article.title,
+        "article_publish_date": article.publish_date
+    }
+    kb = from_text_to_kb(article.text, article.url, **config)
+    return kb
+
+def get_news_links(query, lang="en", region="US", pages=1, max_links=10000):
+    googlenews = GoogleNews(lang=lang, region=region)
+    googlenews.search(query)
+    all_urls = []
+    for page in range(pages):
+        googlenews.get_page(page)
+        all_urls += googlenews.get_links()
+        # Remove query parameters and prefix from URLs
+        clean_urls = [url.split('url=')[1].split('&ved=')[0] for url in all_urls]
+    return list(set(clean_urls))[:max_links]
+
+def from_urls_to_kb(urls, verbose=False):
+    kb = KB()
+    if verbose: 
+        print(f"{len(urls)} links to visit")
+    for url in urls:
+        if verbose:
+            print(f"Visiting {url}")
+        try:
+            kb_url = from_url_to_kb(url)
+            kb.merge_with_kb(kb_url)
+        except ArticleException as e:
+            if verbose:
+                print(f" Couldn't download at URL: {url} Error: {e}")
+    return kb
+
+
+def save_network_html(kb, filename="network.html"):
+    # create network
+    net = Network(directed=True, width="700px", height="700px", bgcolor="#eeeeee")
+
+    # nodes
+    color_entity = "#d5f5e3"
+    for e in kb.entities:
+        net.add_node(e, shape="circle", color=color_entity)
+
+    # edges
+    for r in kb.relations:
+        net.add_edge(r["head"], r["tail"],
+                    title=r["type"], label=r["type"])
+        
+    # save network
+    net.repulsion(
+        node_distance=200,
+        central_gravity=0.2,
+        spring_length=200,
+        spring_strength=0.05,
+        damping=0.09
+    )
+    net.set_edge_smooth('dynamic')
+    net.show(filename)
+
+news_links = get_news_links("UX design interview", pages=2, max_links=20)
+kb = from_urls_to_kb(news_links, verbose=True)
+filename = "UX_Network_821.html"
+save_network_html(kb, filename=filename)
+IPython.display.HTML(filename=filename)
